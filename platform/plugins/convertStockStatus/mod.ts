464ebecf3daf562406c3cd008c258577f3f3de31
Ami Berman <aberman@yext.com>
2021-10-22 14:18:00 -0400

    Add new set of fields/availability transform

---
+++ platform/plugins/convertStockStatus/mod.ts
@@ -0,0 +1,12 @@
+export function convertStockStatus(availability: string) {
+	switch(availability) {
+		case 'in stock':
+			return "IN_STOCK";
+		case 'out of stock':
+			return "OUT_OF_STOCK";
+		case 'preorder':
+			return "PRE_ORDER"
+		default:
+			throw new TypeError('Invalid availability type.');
+	}
+}
